8dee6931b2806549d08400d26cdb2ea175d42158
MrPillow3344 <nevoamiric@gmail.com>
2025-02-17 22:10:37 +0200

    giant

---
+++ src/main/java/net/mrpillow/clashcraft/init/ClashCraftModEntities.java
@@ -0,0 +1,45 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mrpillow.clashcraft.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+import net.mrpillow.clashcraft.entity.GiantEntity;
+import net.mrpillow.clashcraft.ClashCraftMod;
+
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.core.registries.Registries;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+public class ClashCraftModEntities {
+	public static final DeferredRegister<EntityType<?>> REGISTRY = DeferredRegister.create(Registries.ENTITY_TYPE, ClashCraftMod.MODID);
+	public static final DeferredHolder<EntityType<?>, EntityType<GiantEntity>> GIANT = register("giant",
+			EntityType.Builder.<GiantEntity>of(GiantEntity::new, MobCategory.MONSTER).setShouldReceiveVelocityUpdates(true).setTrackingRange(64).setUpdateInterval(3)
+
+					.sized(0.6f, 1.8f));
+
+	// Start of user code block custom entities
+	// End of user code block custom entities
+	private static <T extends Entity> DeferredHolder<EntityType<?>, EntityType<T>> register(String registryname, EntityType.Builder<T> entityTypeBuilder) {
+		return REGISTRY.register(registryname, () -> (EntityType<T>) entityTypeBuilder.build(registryname));
+	}
+
+	@SubscribeEvent
+	public static void init(RegisterSpawnPlacementsEvent event) {
+		GiantEntity.init(event);
+	}
+
+	@SubscribeEvent
+	public static void registerAttributes(EntityAttributeCreationEvent event) {
+		event.put(GIANT.get(), GiantEntity.createAttributes().build());
+	}
+}
---
+++ src/main/java/net/mrpillow/clashcraft/init/ClashCraftModEntityRenderers.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mrpillow.clashcraft.init;
+
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.Dist;
+
+import net.mrpillow.clashcraft.client.renderer.GiantRenderer;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClashCraftModEntityRenderers {
+	@SubscribeEvent
+	public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+		event.registerEntityRenderer(ClashCraftModEntities.GIANT.get(), GiantRenderer::new);
+	}
+}
