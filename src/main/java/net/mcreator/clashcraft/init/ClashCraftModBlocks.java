26ec99f567233d65ff959918a263662046a155c0
MrPillow3344 <nevoamiric@gmail.com>
2025-02-17 15:30:40 +0200

    הוספתי את המימד פורטל למימד ושמתי את הביום במימד

---
+++ src/main/java/net/mcreator/clashcraft/init/ClashCraftModBlocks.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.clashcraft.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.clashcraft.block.ClashDimensionPortalBlock;
+import net.mcreator.clashcraft.ClashCraftMod;
+
+public class ClashCraftModBlocks {
+	public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks(ClashCraftMod.MODID);
+	public static final DeferredBlock<Block> CLASH_DIMENSION_PORTAL = REGISTRY.register("clash_dimension_portal", ClashDimensionPortalBlock::new);
+	// Start of user code block custom blocks
+	// End of user code block custom blocks
+}
