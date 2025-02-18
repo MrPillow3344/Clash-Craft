9991b9fa401069b46a3d6468bcd03027ed4b86ea
Giganto6 <gitay@Gitay.home>
2025-02-18 20:47:37 +0200

    עשיתי כרטיס שאיתו מדליקים את הפורטל

--- src/main/java/net/mrpillow/clashcraft/init/ClashCraftModItems.java
+++ src/main/java/net/mrpillow/clashcraft/init/ClashCraftModItems.java
@@ -8,12 +8,14 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 
+import net.mrpillow.clashcraft.item.ClashDimensionItem;
 import net.mrpillow.clashcraft.ClashCraftMod;
 
 import net.minecraft.world.item.Item;
 
 public class ClashCraftModItems {
 	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(ClashCraftMod.MODID);
+	public static final DeferredItem<Item> CLASH_DIMENSION = REGISTRY.register("clash_dimension", ClashDimensionItem::new);
 	public static final DeferredItem<Item> GIANT_SPAWN_EGG = REGISTRY.register("giant_spawn_egg", () -> new DeferredSpawnEggItem(ClashCraftModEntities.GIANT, -10079488, -26368, new Item.Properties()));
 	// Start of user code block custom items
 	// End of user code block custom items
