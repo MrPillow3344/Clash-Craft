a1395d87f4f6f01586161dda4d846f0395af3fe0
MrPillow3344 <nevoamiric@gmail.com>
2025-02-28 19:32:53 +0200

    גיתאי החצים עובדים

--- src/main/java/net/mrpillow/clashcraft/procedures/SpawnArrowsProcedure.java
+++ src/main/java/net/mrpillow/clashcraft/procedures/SpawnArrowsProcedure.java
@@ -1,5 +1,8 @@
 package net.mrpillow.clashcraft.procedures;
 
+import net.mrpillow.clashcraft.ClashCraftMod;
+
+import net.mrpillow.clashcraft.ClashCraftMod;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
@@ -17,9 +20,8 @@ public class SpawnArrowsProcedure {
 		double xOffset = 6;
 		double yOffset = 20;
 		double r = 3;
-		for (int i = 0; i < 3; i++) {
-		
-			for (int xLoop = 0; xLoop <= (int) (r * 2); xLoop++) {
+
+		for (int xLoop = 0; xLoop <= (int) (r * 2); xLoop++) {
 				for (int zLoop = 0; zLoop <= (int) (r * 2); zLoop++) {
 				
 					//distance
@@ -51,8 +53,8 @@ public class SpawnArrowsProcedure {
 									return entityToSpawn;
 								}
 							}.getArrow(projectileLevel, 2, 1, (byte) 0);
-							_entityToSpawn.setPos(x + xLoop - r + 0.5, (y + yOffset), z+ zLoop - r + 0.5);
-							_entityToSpawn.shoot((xOffset / yOffset), (-1), 1, (float) 1.2, (float) 0.1);
+							_entityToSpawn.setPos(x + xLoop - r - 0.5 -xOffset, (y + yOffset), z- zLoop + r - 0.5);
+							_entityToSpawn.shoot((xOffset / yOffset), (-1), 0, (float) 1.2, (float) 0.1);
 							projectileLevel.addFreshEntity(_entityToSpawn);
 						}
 
@@ -60,6 +62,52 @@ public class SpawnArrowsProcedure {
 					}
 				}
 			}
+		
+		for (int i = 0; i < 2; i++) {
+			ClashCraftMod.queueServerWork(9, () -> {
+				for (int xLoop = 0; xLoop <= (int) (r * 2); xLoop++) {
+				for (int zLoop = 0; zLoop <= (int) (r * 2); zLoop++) {
+				
+					//distance
+					if (dSquared(xLoop, zLoop, r, r) <= Math.pow(r, 2)) {
+
+						//Spawn Arrows
+						if (world instanceof ServerLevel projectileLevel) {
+							Projectile _entityToSpawn = new Object() {
+								public Projectile getArrow(Level level, float damage, int knockback, byte piercing) {
+									AbstractArrow entityToSpawn = new Arrow(EntityType.ARROW, level) {
+										@Override
+										public byte getPierceLevel() {
+											return piercing;
+										}
+
+										@Override
+										protected void doKnockback(LivingEntity livingEntity, DamageSource damageSource) {
+											if (knockback > 0) {
+												double d1 = Math.max(0.0, 1.0 - livingEntity.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
+												Vec3 vec3 = this.getDeltaMovement().multiply(1.0, 0.0, 1.0).normalize().scale(knockback * 0.6 * d1);
+												if (vec3.lengthSqr() > 0.0) {
+													livingEntity.push(vec3.x, 0.1, vec3.z);
+												}
+											}
+										}
+									};
+									entityToSpawn.setBaseDamage(damage);
+									entityToSpawn.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+									return entityToSpawn;
+								}
+							}.getArrow(projectileLevel, 2, 1, (byte) 0);
+							_entityToSpawn.setPos(x + xLoop - r - 0.5 -xOffset, (y + yOffset), z- zLoop + r - 0.5);
+							_entityToSpawn.shoot((xOffset / yOffset), (-1), 0, (float) 1.2, (float) 0.1);
+							projectileLevel.addFreshEntity(_entityToSpawn);
+						}
+
+						
+					}
+				}
+			}
+				
+			});
 
 			
 		}
