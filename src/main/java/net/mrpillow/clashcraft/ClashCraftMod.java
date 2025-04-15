a06f81c9da8561980c26c4d012760426c8e0a7a0
gitay <gitay@Gitay.home>
2025-04-15 22:58:39 +0300

    MCreator Merge commit '17ef6fa6b61b9ce7105d68c5dc7b7b44bd6164c9' into 904d2bcacb505a247452d320485af9b90ae077e6

--- src/main/java/net/mrpillow/clashcraft/ClashCraftMod.java
+++ src/main/java/net/mrpillow/clashcraft/ClashCraftMod.java
@@ -21,7 +21,6 @@ import net.mrpillow.clashcraft.init.ClashCraftModMobEffects;
 import net.mrpillow.clashcraft.init.ClashCraftModItems;
 import net.mrpillow.clashcraft.init.ClashCraftModEntities;
 import net.mrpillow.clashcraft.init.ClashCraftModBlocks;
-import net.mrpillow.clashcraft.init.ClashCraftModAttributes;
 
 import net.minecraft.util.Tuple;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -58,7 +57,6 @@ public class ClashCraftMod {
 
 		ClashCraftModParticleTypes.REGISTRY.register(modEventBus);
 
-		ClashCraftModAttributes.REGISTRY.register(modEventBus);
 		// Start of user code block mod init
 		// End of user code block mod init
 	}
